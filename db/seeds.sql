a54763b1614e021c27fc7e7cdc189fd13c5c6e7e
Clay Hibbard <38364771+Hibbarcd@users.noreply.github.com>
2018-10-13 15:38:15 -0500

    Add files via upload
    Intial commit after heroku issues

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, hungry) VALUES ('Bacon Bleu', true);
+INSERT INTO burgers (name, hungry) VALUES ('Flamin Cajun', true);
+
